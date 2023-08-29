84d5cb81c59f1865c682e2db70a94cf1dcf051e5
Bianca Calderón <98110978+BiancaCalderon@users.noreply.github.com>
2023-08-29 16:06:43 -0600

    Fixed First Commit

---
+++ app/src/main/java/com/example/lab5biancacalderon/Event.kt
@@ -0,0 +1,11 @@
+package com.example.lab5biancacalderon
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Event(
+    var name: String,
+    val availableSeats: Int,
+
+)
+
